eafd71dd39b300790163475f142a33bc36f4814e
adarshtri <adarsh.trivedi100@gmail.com>
2020-04-21 14:55:32 -0400

    Setup file for VCL

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo yum install python3
+sudo yum install firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=5000/tcp
+sudo firewall-cmd --reload
+sudo yum install git
